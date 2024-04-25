d5e2b03dfad7ce1da65294d68c3089ae7ce69093
David Lidueña <davidliduena13@gmail.com>
2024-04-25 12:06:06 -0500

    advances

---
+++ src/routes/category.router.js
@@ -0,0 +1,14 @@
+const { getAll, create, remove } = require('../controllers/category.controllers');
+const express = require('express');
+const { verifyJwt } = require('../utils/verifyJWT');
+
+const routerCategory = express.Router();
+
+routerCategory.route('/')
+    .get(getAll)
+    .post(verifyJwt, create); //🗝️
+
+routerCategory.route('/:id')
+    .delete(verifyJwt, remove)//🗝️
+
+module.exports = routerCategory;
