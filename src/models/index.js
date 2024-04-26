fe24d3586b02d4c66d919675469e898b4f373647
David Lidueña <davidliduena13@gmail.com>
2024-04-26 15:17:34 -0500

    advances

--- src/models/index.js
+++ src/models/index.js
@@ -1,3 +1,19 @@
 const User = require("./User")
-const category = require("./Category")
+const Category = require("./Category")
 const Product = require('./Product')
+const Cart = require("./Cart")
+
+
+//Product -> categoryId
+Product.belongsTo(Category)
+Category.hasMany(Product)
+
+
+//?Cart ->userId
+Cart.belongsTo(User)
+User.hasMany(Cart)
+
+
+//!Cart -> productId
+Cart.belongsTo(Product)
+Product.hasMany(Cart)
