755abd6f31e4b5c43a3a33636882d0b02ec42fe7
David Lidueña <davidliduena13@gmail.com>
2024-04-29 12:40:56 -0500

    advances

--- src/models/index.js
+++ src/models/index.js
@@ -2,18 +2,24 @@ const User = require("./User")
 const Category = require("./Category")
 const Product = require('./Product')
 const Cart = require("./Cart")
-
+const Purchase = require("./Purchase")
 
 //Product -> categoryId
 Product.belongsTo(Category)
 Category.hasMany(Product)
 
-
 //?Cart ->userId
 Cart.belongsTo(User)
 User.hasMany(Cart)
 
-
 //!Cart -> productId
 Cart.belongsTo(Product)
 Product.hasMany(Cart)
+
+//?Purchase -> UserId
+Purchase.belongsTo(User)
+User.hasMany(Purchase)
+
+//!Purchase -> productId
+Purchase.belongsTo(Product)
+Product.hasMany(Purchase)
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,14 @@ const routerCategory = require('./category.router');
 const routerProduct = require('./product.router');
 const routerCart = require('./cart.router');
 const { verifyJwt } = require('../utils/verifyJWT');
+const routerPurchase = require('./purchase.router');
 const router = express.Router();
 
 // colocar las rutas aquí
 router.use('/users', routerUser)
 router.use('/categories', routerCategory)
 router.use('/products', routerProduct)
-router.use('/carts', verifyJwt, routerCart)//todas las rutas quedan protegidas🗝️🗝️🗝️🗝️🗝️
+router.use('/cart', verifyJwt, routerCart)//todas las rutas quedan protegidas🗝️🗝️🗝️🗝️🗝️
+router.use('/purchase', verifyJwt, routerPurchase)//todas las rutas quedan protegidas🗝️🗝️
 
 module.exports = router;
